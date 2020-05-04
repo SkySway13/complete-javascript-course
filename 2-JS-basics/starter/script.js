e77db6758b0b27711a45f6df90619c4f7aff9707
Nianchen Shen <Nianchen.Shen@Outlook.com>
2020-05-04 17:16:48 +0800

    End of S2-10

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,1 +1,63 @@
-console.log('Hello World');
+/* var firstName = 'John'
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
+ */
+
+var firstName = 'John';
+var age = 28;
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+var ms
+function CheckMarrigeStatus() {
+    if (isMarried == false) {
+        ms = 'No'
+    }
+    else {
+        ms = 'Yes'
+    }
+};
+
+CheckMarrigeStatus();
+
+alert(firstName + ' is a ' + age + ' years old '
+    + job + '. Is he married? ' + ms);
+
+age = 'Zero';
+job = 'Who knows';
+
+var lastName = prompt('Test');
+console.log(lastName);
+
+
+var currentYear = 2020;
+var birthYear = currentYear - 28;
+console.log(birthYear);
+
+var a = 10;
+var b = 200;
+
+var isLarger = a > b;
+
+console.log(isLarger);
+console.log(typeof a);
+console.log(typeof isLarger);
+console.log(typeof 1);
+console.log(typeof 'test')
+var x;
+console.log(typeof x);
+
